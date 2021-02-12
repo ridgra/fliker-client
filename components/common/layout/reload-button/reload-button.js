7fb6b37323ddf5f2b35c9f7d96b0dd11fd44bf89
Ridho Anugrah <im.ridgra@gmail.com>
2021-02-13 00:55:11 +0700

    reload-button.js
    rename components

--- components/common/layout/reload-button/reload-button.js
+++ components/common/layout/reload-button/reload-button.js
@@ -2,7 +2,7 @@ import cn from '../../../../utils/cn';
 import styles from './reload-button.module.scss';
 import IconReload from '../../../../assets/icon-reload.svg';
 
-export default function Headings({ children }) {
+export default function ReloadButton({ children }) {
   return (
     <button {...cn(styles.button)}>
       <IconReload />
