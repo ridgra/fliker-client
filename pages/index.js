2c785c89c2b8eaf3822819c313fa8da15cb07cd8
Ridho Anugrah <im.ridgra@gmail.com>
2021-02-13 04:15:11 +0700

    insert components into home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head';
 import Layout from '../components/common/layout';
-import Main from '../components/home';
+import Contents from '../components/home/contents';
+import Headings from '../components/home/headings';
+import Pagination from '../components/home/pagination';
+import styles from '../styles/home.module.scss'
+import cn from '../utils/cn';
 
 export default function Home() {
   return (
-    <Layout>
-     
-      <Main />
+    <Layout {...cn(styles.home)}>
+      <Headings />
+      <Contents />
+      <Pagination />
     </Layout>
   );
 }
